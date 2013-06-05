7dc5fbbb473d5c1eaf9d98fbd9a9d5db608fe391
maoer0317 <chenx@tom.com>
2013-06-05 14:40:52 +0800

    修改：      test/splitstr.c

--- test/splitstr.c
+++ test/splitstr.c
@@ -46,17 +46,13 @@ int is_pair(const char *s)
 //class B
 //judge if the string is a pair 
 {
-  char c1,c2;
   if(strlen(s)!=2)
     return 0;
   
-  c1=*s;
-  c2=*(s+1);
-  //printf("%c,%c\n",c1,c2);
-  if(c1!=c2)
-    return 0;
+  if(*s==*(s+1))
+    return 1;
   else 
-    return 1;  
+    return 0;  
 }
 
 int is_triple(const char *s)
@@ -121,32 +117,14 @@ void str_split(const char *s)
   sp_table *sp=NULL;
   
   split_1234(s,1,&sp);
-  if(sp==NULL)
-	printf ("%s\n","sp==NULL");
+  //if(sp==NULL)
+  //printf ("%s\n","sp==NULL");
   print_sp(sp);
   add_sp(sp);
   
-  //split to pairs, class B
-  /*for(i=0;i<strlen(s)-1;i++){
-    strncpy(s1,s+i,2);
-    *(s1+2)='\0';
-    //printf("%s,%d\n",s1,i);
-    if(is_pair(s1) && !find_str(s1))
-       add_to_str(s1);
-  }
-
-  //split to triple, class C
-  for(i=0;i<strlen(s)-2;i++){
-    strncpy(s1,s+i,3);
-    *(s1+3)='\0';
-    //printf("%s,%d\n",s1,i);
-    if(is_triple(s1) && !find_str(s1))
-       add_to_str(s1);
-  }
-  */
-  //split to triple+one, class D
+  *sp=NUll;
+  split_1234(s,2,&sp);
   
-
   printf("\n");
 }
 
